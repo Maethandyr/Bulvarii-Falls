f4341a857cc42a8745aa3b6eb0368d7dfd31e886
Alex Chheng <Alex.Chheng@student.csulb.edu>
2022-03-14 02:09:56 -0700

    Add movement and king
    Movement script
    king art

--- Bulvarii-Falls/Assets/Scripts/Movement.cs
+++ Bulvarii-Falls/Assets/Scripts/Movement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float speed = 8; // players move speed
+
+    Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
