94bfcbae43a838c817882bfa581c6b172f93ef91
KJackson159 <75142719+KJackson159@users.noreply.github.com>
2022-04-01 20:33:42 -0700

    added object assets placeholders

--- Bulvarii-Falls/Assets/Scripts/WaterfallInteract.cs
+++ Bulvarii-Falls/Assets/Scripts/WaterfallInteract.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterfallInteraction : MonoBehaviour
+public class WaterfallInteract : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
