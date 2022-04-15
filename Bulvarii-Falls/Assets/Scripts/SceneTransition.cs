19813b8fdb07e09efb41aa11af636002ba7e30c8
Maethandyr <Alejandro.Rodriguez03@student.csulb.edu>
2022-04-14 23:45:35 -0700

    Death Text

--- Bulvarii-Falls/Assets/Scripts/SceneTransition.cs
+++ Bulvarii-Falls/Assets/Scripts/SceneTransition.cs
@@ -16,7 +16,7 @@ public class SceneTransition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) || timer >= 0)
+        if (Input.GetMouseButtonDown(0) || timer <= 0)
         {
             SceneManager.LoadScene(sceneBuildIndex);
         }
