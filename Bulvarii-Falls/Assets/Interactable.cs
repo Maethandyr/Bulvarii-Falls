c04385d64ae47844163d072506d66c44e0d9b2b2
Maethandyr <Alejandro.Rodriguez03@student.csulb.edu>
2022-04-17 20:11:03 -0700

    Added a sketch of map

--- Bulvarii-Falls/Assets/Interactable.cs
+++ Bulvarii-Falls/Assets/Interactable.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+{
+    private GameObject rock;
+
+
+        //Update is called once per frame
+ void Update()
+        {
+
+        }
+    
+
+    void OnTriggerStay2D(Collider2D other)
+
+        {
+            
+            other.attachedRigidbody.AddForce(-0.1F * other.attachedRigidbody.velocity);
+        }
 }
