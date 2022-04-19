9d9a57bc7bac790459c4ea6d2992c3b93b941310
Maethandyr <Alejandro.Rodriguez03@student.csulb.edu>
2022-04-18 18:45:57 -0700

    Progress of Interactable Pause Menu

--- Bulvarii-Falls/Assets/Interactable.cs
+++ Bulvarii-Falls/Assets/Interactable.cs
@@ -1,24 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
-
 {
-    private GameObject rock;
+    public bool isGamePaused;
+    public GameObject thisIsMyText; //Drop specific memory text here OR
+    public TextMeshPro TMP; //here
 
+    public PauseMenu pauseMenu;
 
-        //Update is called once per frame
- void Update()
+    void Update ()
+    {
+        if (isGamePaused && Input.GetKeyDown("e"))
         {
-
+            pauseMenu.Resume();
+            LeaveMeText();
         }
-    
+    }
+  
 
-    void OnTriggerStay2D(Collider2D other)
+    void GiveMeText() 
 
-        {
-            
-            other.attachedRigidbody.AddForce(-0.1F * other.attachedRigidbody.velocity);
-        }
-}
+    {
+        Debug.Log("You can talk to rocks now!");
+        //TMP.SetActive(true);
+    }
+    void LeaveMeText()
+    {
+    //TMP.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+    if (other.CompareTag("Player") && Input.GetKeyDown("e"))
+    {
+        // Show to player "Press e to continue
+        //Pause();
+        GiveMeText();
+    }
+    }
+} 
